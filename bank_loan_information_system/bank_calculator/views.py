ffcf2327fd1e897a36ac6ff5b2439bba7e91e352
Tristan <tlbachini@up.edu.ph>
2022-05-26 10:13:00 +0800

    finished bank loan calculator function, modified and completed authentication function.

--- bank_loan_information_system/bank_calculator/views.py
+++ bank_loan_information_system/bank_calculator/views.py
@@ -1,27 +1,41 @@
 from django.shortcuts import render
-
 from bank_calculator.models import Months_To_Pay
 
 # Create your views here.
-def loan_compute(percentage,selling_price,amount,number_of_months):
-    
+def pmt(percentage, loan, number_of_months):
 
-    
+    number_of_months = int(number_of_months)
+    #interest rate is 5.25%
+    #formula for pmt used in spreadsheet
+    monthly_payment = (loan*(0.0525/12))/(1-(1+(0.0525/12))**-(12*(number_of_months/12)))
+
     return monthly_payment
+
+
+
+
+def loan_compute(percentage,amount,preferred_unit,selling_price,loan,number_of_months,months):
+
+    monthly_payment = pmt(percentage,loan,number_of_months)
+    summary  = {'number_of_months':number_of_months,'percentage':percentage,'payment':monthly_payment,
+    'amount':amount,'unit':preferred_unit,'price':selling_price,'loan':loan,'months':months}
+    
+    return summary
 
 def loan_calculator(request):
     months = Months_To_Pay.objects.all()
-
+    
     if(request.method == 'POST'):
+
         percentage=request.POST.get('percentage')
         selling_price=request.POST.get('sellingprice')
         amount=request.POST.get('amount')
         number_of_months=request.POST.get('months')
         preffered_unit=request.POST.get('unit')
+        amount_financed = float(selling_price) - float(amount)
 
-        if(percentage or selling_price or amount or number_of_months or preffered_unit is None):
-            monthly_payment = loan_compute(percentage,selling_price,amount,number_of_months)
-            monthly_payment = (selling_price/number_of_months) + (selling_price/number_of_months)*.20
-            data = {'monthly_payment':monthly_payment,'months':number_of_months}
+        data = loan_compute(percentage,amount,preffered_unit,selling_price,amount_financed,number_of_months,months)
+
+        return render(request, 'bank_calculator/loancalculator.html',data)
 
     return render(request, 'bank_calculator/loancalculator.html',{'months':months})
