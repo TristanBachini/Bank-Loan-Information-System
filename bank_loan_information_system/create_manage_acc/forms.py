23d9499b1dd6ab113282b9356b4a0e2fa109c163
Mirai Yoshizaki <mryoshizaki@up.edu.ph>
2022-05-29 04:29:55 +0800

    updated input form

--- bank_loan_information_system/create_manage_acc/forms.py
+++ bank_loan_information_system/create_manage_acc/forms.py
@@ -5,6 +5,27 @@ TYPE_SELECT = (
     ('1', 'Male'),
 )
 
-class AccountRegForm(forms.Form):
+MARITAL_STATUS = (
+    ('0', 'Single'),
+    ('1', 'Married'),
+    ('2', 'Divorced'),
+    ('3', 'Widow'),
+)
 
+YEARS= [x for x in range(1940,2022)]
+
+
+class AccountRegForm(forms.Form):
+    first_name = forms.CharField(label='First name', max_length=100)
+    last_name = forms.CharField(label='Last name', max_length=100)
     sex = forms.CharField(label='Sex', widget=forms.RadioSelect(choices=TYPE_SELECT))
+    marital_status = forms.CharField(label='Marital Status', widget=forms.RadioSelect(choices=MARITAL_STATUS))
+    birth_date= forms.DateField(label='What is your birth date?', initial="1990-06-21", widget=forms.SelectDateWidget(years=YEARS))
+    email = forms.EmailField(
+        label=("E-mail"),
+        required=True,
+        widget=forms.TextInput(
+            attrs={"type": "email",
+                   "size": "30",
+                   "placeholder":('E-mail address')})
+    )
