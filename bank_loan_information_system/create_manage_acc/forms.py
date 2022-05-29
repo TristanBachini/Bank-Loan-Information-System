efaa69853e1fbac8ef19020aad3eee4f4f21aec4
Mirai Yoshizaki <mryoshizaki@up.edu.ph>
2022-05-29 17:12:28 +0800

    edited html for create-manage

--- bank_loan_information_system/create_manage_acc/forms.py
+++ bank_loan_information_system/create_manage_acc/forms.py
@@ -1,42 +1,55 @@
 from django import forms
-from phonenumber_field.formfields import PhoneNumberField
+from django.forms import ModelForm, TextInput, PasswordInput, CharField, HiddenInput, NumberInput,CheckboxInput
+from .models import AccountReg
+from django.forms.widgets import DateInput, Select, RadioSelect
 import datetime
 
-TYPE_SELECT = (
-    ('0', 'Female'),
-    ('1', 'Male'),
-)
+# TYPE_SELECT = (
+#     ('0', 'Female'),
+#     ('1', 'Male'),
+# )
 
-MARITAL_STATUS = (
-    ('0', 'Single'),
-    ('1', 'Married'),
-    ('2', 'Divorced'),
-    ('3', 'Widow'),
-)
+# MARITAL_STATUS = (
+#     ('0', 'Single'),
+#     ('1', 'Married'),
+#     ('2', 'Divorced'),
+#     ('3', 'Widow'),
+# )
 
-PREFIX = (
-    ('0', 'Mr.'),
-    ('1', 'Mrs.'),
-    ('2', 'Ms.'),
-)
+# PREFIX = (
+#     ('0', 'Mr.'),
+#     ('1', 'Mrs.'),
+#     ('2', 'Ms.'),
+# )
 
-YEARS= [x for x in range(1940,2022)]
+# YEARS= [x for x in range(1940,2022)]
 
 
-class AccountRegForm(forms.Form):
-    first_name = forms.CharField(label='First name', max_length=100)
-    last_name = forms.CharField(label='Last name', max_length=100)
-    sex = forms.CharField(label='Sex', widget=forms.RadioSelect(choices=TYPE_SELECT))
-    marital_status = forms.CharField(label='Marital Status', widget=forms.RadioSelect(choices=MARITAL_STATUS))
-    birth_date= forms.DateField(label='What is your birth date?', initial="1990-06-21", widget=forms.SelectDateWidget(years=YEARS))
-    email = forms.EmailField(
-        label=("E-mail"),
-        required=True,
-        widget=forms.TextInput(
-            attrs={"type": "email",
-                   "size": "30",
-                   "placeholder":('E-mail address')})
-    )
-    prefix = forms.CharField(label='Prefix', widget=forms.Select(choices=PREFIX))
-    phone = PhoneNumberField()
-    address = forms.CharField(label='Last name', max_length=300)
+class AccountRegForm(ModelForm):
+    # first_name = forms.CharField(label='First name', max_length=100)
+    # last_name = forms.CharField(label='Last name', max_length=100)
+    # sex = forms.CharField(label='Sex', widget=forms.RadioSelect(choices=TYPE_SELECT))
+    # marital_status = forms.CharField(label='Marital Status', widget=forms.RadioSelect(choices=MARITAL_STATUS))
+    # birth_date= forms.DateField(label='What is your birth date?', initial="1990-06-21", widget=forms.SelectDateWidget(years=YEARS))
+    # email = forms.EmailField(
+    #     label=("E-mail"),
+    #     required=True,
+    #     widget=forms.TextInput(
+    #         attrs={"type": "email",
+    #                "size": "30",
+    #                "placeholder":('E-mail address')})
+    # )
+    # prefix = forms.CharField(label='Prefix', widget=forms.Select(choices=PREFIX))
+    # phone = PhoneNumberField()
+    # address = forms.CharField(label='Last name', max_length=300)
+    class Meta:
+        model = AccountReg
+        fields = "__all__"
+        widgets = {
+            'user':  HiddenInput(attrs={'type': 'hidden'}),
+            'first_name' : forms.TextInput(attrs={'class': 'form-control input-area', 'placeholder': 'Enter your name', 'required' : True}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last name', 'aria-label': 'Last name', 'required': True}),
+            'prefix':  Select(attrs={'class': 'form-control'}),
+            'sex':  Select(attrs={'class': 'form-control'}),
+            'marital_status':  Select(attrs={'class': 'form-control'})
+        }
--- bank_loan_information_system/create_manage_acc/models.py
+++ bank_loan_information_system/create_manage_acc/models.py
@@ -1,1 +1,40 @@
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.formfields import PhoneNumberField
+import datetime
+
+class AccountReg(models.Model):
+    SEX = (
+        ('F', 'Female'),
+        ('M', 'Male'),
+    )
+
+    MARITAL_STATUS = (
+        ('S', 'Single'),
+        ('M', 'Married'),
+        ('D', 'Divorced'),
+        ('W', 'Widow'),
+    )
+
+    PREFIX = (
+        ('MR', 'Mr.'),
+        ('MRS', 'Mrs.'),
+        ('MS', 'Ms.'),
+    )
+
+    YEARS= [x for x in range(1940,2022)]
+
+    user = models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    sex = models.CharField(max_length=100, choices=SEX)
+    marital_status = models.CharField(max_length=100, choices=MARITAL_STATUS)
+    birth_date= models.DateField(blank=True, null=True)
+    email = models.EmailField()
+    prefix = models.CharField(max_length=100, choices=PREFIX)
+    phone = PhoneNumberField()
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return '{} - {}'.format(self.user,self.email)
